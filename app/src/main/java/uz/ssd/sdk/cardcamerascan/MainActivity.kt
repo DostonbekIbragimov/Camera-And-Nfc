a39b6f156ec62badc045eb45adb7e5536d863b8b
DostonbekIbragimov <idostonbek1230@mail.ru>
2021-07-12 14:34:51 +0500

    nfc permission

--- app/src/main/java/uz/ssd/sdk/cardcamerascan/MainActivity.kt
+++ app/src/main/java/uz/ssd/sdk/cardcamerascan/MainActivity.kt
@@ -1,13 +1,15 @@
 package uz.ssd.sdk.cardcamerascan
 
 import android.content.Intent
+import android.nfc.NfcAdapter
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import uz.ssd.sdk.camerascan.CardScanActivity
 import uz.ssd.sdk.nfc.NfcActivity
-
-//import uz.ssd.sdk.nfc.NFCActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +17,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         nfc.setOnClickListener {
-            startActivity(Intent(this, CardScanActivity::class.java))
+            nfcOpen()
+
         }
 
-       /* camera.setOnClickListener {
+        camera.setOnClickListener {
             startActivity(Intent(this, CardScanActivity::class.java))
-        }*/
+        }
+    }
+
+    private fun nfcOpen() {
+        val mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        if (!mNfcAdapter.isEnabled) {
+            val alertbox: AlertDialog.Builder = AlertDialog.Builder(this)
+            alertbox.setTitle("Info")
+            alertbox.setMessage(getString(R.string.msg_nfcon))
+            alertbox.setPositiveButton("Turn On") { dialog, which ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    val intent = Intent(Settings.ACTION_NFC_SETTINGS)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                    startActivity(intent)
+                }
+            }
+            alertbox.setNegativeButton("Close") { dialog, which ->
+
+            }
+            alertbox.show()
+        } else {
+            startActivity(Intent(this, NfcActivity::class.java))
+        }
     }
 }
